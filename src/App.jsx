b25dd0cf7ed35cedbe99121626b48d8d2d07d719
sebahattin <sebkucuk05@gmail.com>
2023-02-07 00:20:15 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      🍀 This Component App.jsx
+      <h1>Pokemon Go Card Game</h1>
+    </div>
+  )
+}
+
+export default App
