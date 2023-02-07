ff141e4600eb04935d988463c4a325b5835a3ffd
sebahattin <sebkucuk05@gmail.com>
2023-02-08 01:14:23 +0300

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
+import "./Pokecard.css"
 import './App.css'
+import Pokedex from "./Pokedex"
+
 
 function App() {
 
@@ -7,6 +10,7 @@ function App() {
     <div className="App">
       🍀 This Component App.jsx
       <h1>Pokemon Go Card Game</h1>
+      <Pokedex />
     </div>
   )
 }
--- src/Pokecard.jsx
+++ src/Pokecard.jsx
@@ -1,1 +1,27 @@
 import React, { Component } from "react";
+
+
+const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/'
+
+class Pokecard extends Component {
+
+	render() {
+		let imgSrc = `${POKE_API}${this.props.idPost}.png`
+		return (
+			<div className='Pokecard'>
+				🍀 This Component Pokecard.jsx
+				<h1 className="Pokecard-title">Pokemon</h1>
+				<div className="Pokecard-image">
+					<img src={imgSrc} alt={this.props.namePost} />
+				</div>
+				<div className="Pokecard-data"> Name: {this.props.namePost}</div>
+				<div className="Pokecard-data"> Type: {this.props.typePost}</div>
+				{/* <div className="Pokecard-data2"> Exp: {this.props.base_experience}</div> */}
+				<div className="Pokecard-data"> Exp: {this.props.expPost}</div>
+				{/* type={p.type} exp={p.base_experience} */}
+			</div>
+		)
+	}
+}
+
+export default Pokecard
